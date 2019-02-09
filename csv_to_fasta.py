1df419a73f62389738b41e464236c0c4b1065003
Niranj Chandrasekaran <Srinivas.Chandrasekaran@umassmed.edu>
2019-02-09 14:34:02 -0500

    cosmetic changes

--- csv_to_fasta.py
+++ csv_to_fasta.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser(description='Converts csv to single line FASTA'
 
 parser.add_argument('-f', metavar='', help='Input csv file')
 parser.add_argument('-n', metavar='', help='Input FASTA names')
-parser.add_argument('-o', metavar='', help= 'output FASTA file')
+parser.add_argument('-o', metavar='', help='output FASTA file')
 
 args = parser.parse_args()
 
